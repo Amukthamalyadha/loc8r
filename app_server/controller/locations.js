14bd3b89b929d13020946fae89a2b08755cbcc39
Amukthamalyadha <amukthamalyadha1393@gmail.com>
2023-08-10 11:13:18 +0530

    first commit

--- app_server/controller/locations.js
+++ app_server/controller/locations.js
@@ -4,6 +4,12 @@ module.exports.homelist=function(req,res){
 module.exports.locationInfo=function(req,res){
     res.render('location-info',{title:'Location Info'});
 };
+module.exports.locationInfo2=function(req,res){
+    res.render('location-info2',{title:'Location Info'});
+};
+module.exports.locationInfo3=function(req,res){
+    res.render('location-info3',{title:'Location Info'});
+};
 module.exports.addReview=function(req,res){
     res.render('location-review-form',{title:'Add Review'});
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,6 +5,8 @@ var ctrlOthers=require('../controller/others')
 
 router.get('/',ctrlLocations.homelist);
 router.get('/location',ctrlLocations.locationInfo);
+router.get('/location2',ctrlLocations.locationInfo2);
+router.get('/location3',ctrlLocations.locationInfo3);
 router.get('/location/review/new',ctrlLocations.addReview);
 
 /*var homepagecontroller=function(req,res)
